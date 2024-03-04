61cd0edcb5d276c8258b4f02955863157d8710e6
leye195 <leye195@naver.com>
2024-03-04 22:52:06 +0900

    eslint rule setting & fix lint error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,13 +4,74 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
     "plugin:react-hooks/recommended",
     "plugin:storybook/recommended",
   ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "eslint-plugin-import"],
   rules: {
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          ["parent", "sibling"],
+          "index",
+        ],
+        pathGroups: [
+          {
+            pattern: "next",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "react",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "@",
+            group: "internal",
+            position: "after",
+          },
+        ],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+    "func-style": ["error", "declaration"],
+    "default-case-last": "error",
+    "no-console": "error",
+    "no-empty": "error",
+    "react/button-has-type": ["error"],
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-sort-props": [
+      "error",
+      {
+        callbacksLast: true,
+        reservedFirst: true,
+      },
+    ],
+    "react/self-closing-comp": [
+      "error",
+      {
+        component: true,
+        html: true,
+      },
+    ],
+    "react/jsx-pascal-case": ["error"],
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
