8206b0bbb0d0221366ace06b4fd40dea6597fd81
leye195 <leye195@naver.com>
2024-03-09 23:37:26 +0900

    feat: add useClickOutSideEffect

---
+++ src/hooks/useClickOutSideEffect.ts
@@ -0,0 +1,47 @@
+import { useCallback, useEffect, useRef } from "react";
+
+type OneOrMore<T> = T | T[];
+
+function useClickOutSideEffect(
+  container: OneOrMore<HTMLElement> | null,
+  callback: VoidFunction,
+) {
+  const containers = useRef<HTMLElement[]>([]);
+
+  const handleClick = useCallback(
+    ({ target }: MouseEvent | TouchEvent) => {
+      function isNotValid() {
+        return (
+          target == null ||
+          containers.current.length === 0 ||
+          containers.current.some((x) => x.contains(target as Node))
+        );
+      }
+
+      if (isNotValid()) return;
+
+      callback();
+    },
+    [callback],
+  );
+
+  useEffect(() => {
+    if (!container) return;
+
+    containers.current = (
+      Array.isArray(container) ? container : [container]
+    ).filter((e) => e != null);
+  }, [container]);
+
+  useEffect(() => {
+    document.addEventListener("click", handleClick);
+    document.addEventListener("touchstart", handleClick);
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+      document.removeEventListener("touchstart", handleClick);
+    };
+  }, [handleClick]);
+}
+
+export default useClickOutSideEffect;
