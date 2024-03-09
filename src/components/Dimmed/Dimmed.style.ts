3f0cf104f6a64cd29f4ed455e81e55e5fb2b1d1a
leye195 <leye195@naver.com>
2024-03-10 00:38:02 +0900

    style: remove opacity from baseStyle

--- src/components/Dimmed/Dimmed.style.ts
+++ src/components/Dimmed/Dimmed.style.ts
@@ -1,2 +1,1 @@
-export const baseStyle =
-  "fixed top-0 left-0 w-full h-full bg-[#000000] opacity-60";
+export const baseStyle = "fixed top-0 left-0 w-full h-full bg-[#000000]";
--- src/components/Dimmed/index.tsx
+++ src/components/Dimmed/index.tsx
@@ -1,14 +1,20 @@
-import { PropsWithChildren } from "react";
+import { CSSProperties, PropsWithChildren } from "react";
 import { baseStyle } from "./Dimmed.style";
 
 interface DimmedProps extends PropsWithChildren {
   className?: string;
+  opacity?: CSSProperties["opacity"];
 }
 
-function Dimmed({ children, className = "" }: DimmedProps) {
+function Dimmed({ children, className = "", opacity = "0.6" }: DimmedProps) {
   return (
     <div>
-      <div className={`${baseStyle} ${className}`} />
+      <div
+        className={`${baseStyle} ${className}`}
+        style={{
+          opacity,
+        }}
+      />
       {children}
     </div>
   );
