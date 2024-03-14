0681485f2c3a033a78ac092648562ac9549d9a11
leye195 <leye195@naver.com>
2024-03-14 23:56:11 +0900

    style: remove colors from baseStyle&inValidStyle

--- src/components/Input/Input.style.ts
+++ src/components/Input/Input.style.ts
@@ -1,9 +1,8 @@
-import { colors } from "@ui-system/styles/colors";
 import { typographyMap } from "@ui-system/styles/typography";
 
-export const baseStyle = `rounded-[10px] border-solid border-[1px]  border-[${colors.gray300}] box-border px-[15px] py-[10px]`;
+export const baseStyle = `rounded-[10px] border-solid border-[1px]  border-[#ebebeb] box-border px-[15px] py-[10px]`;
 
-export const inValidStyle = `rounded-[10px] border-solid border-[1px]  border-[${colors.red200}] box-border px-[15px] py-[10px]`;
+export const inValidStyle = `rounded-[10px] border-solid border-[1px]  border-[#ea3a3d] box-border px-[15px] py-[10px]`;
 
 export const inputSizeMap = {
   small: `h-[40px] ${typographyMap["body-400-5"]}`,
