2a2634f892913aa4d641e46e4b43021117900bf5
leye195 <leye195@naver.com>
2024-03-24 13:34:14 +0900

    feat: set chevron-left to default icon on accordion header

--- src/components/Accordion/accordion-header.tsx
+++ src/components/Accordion/accordion-header.tsx
@@ -1,5 +1,5 @@
+import Flex from "@ui-system/shared/Flex";
 import clsx from "clsx";
-import Flex from "../shared/Flex";
 
 interface AccordionHeaderProps {
   isOpen: boolean;
@@ -15,21 +15,46 @@ function AccordionHeader({
   onClick,
 }: AccordionHeaderProps) {
   return (
-    <header className="py-3" role="button" onClick={onClick}>
+    <header className={"py-3"} role="button" onClick={onClick}>
       <Flex align="center" justify="space-between">
         {children}
-        {icon ? (
-          <span
-            className={clsx(
-              "transition-all",
-              isOpen ? "rotate-180" : "rotate-0",
-            )}
-          >
-            {icon}
-          </span>
-        ) : null}
+
+        <span
+          className={clsx(
+            "transition-all",
+            isOpen ? "rotate-[-90deg]" : "rotate-0",
+          )}
+        >
+          {icon ?? <ChevronLeft />}
+        </span>
       </Flex>
     </header>
+  );
+}
+
+function ChevronLeft() {
+  return (
+    <svg
+      fill="none"
+      height="16"
+      id="chevron-right"
+      style={{
+        transform: "rotate(180deg)",
+      }}
+      viewBox="0 0 16 16"
+      width="16"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="icon/arrow-chevron-right">
+        <path
+          clipRule="evenodd"
+          d="M11.9603 7.46967C12.2532 7.76256 12.2532 8.23744 11.9603 8.53033L6.71026 13.7803C6.41737 14.0732 5.94249 14.0732 5.6496 13.7803C5.35671 13.4874 5.35671 13.0126 5.6496 12.7197L10.3693 8L5.6496 3.28033C5.35671 2.98744 5.35671 2.51256 5.6496 2.21967C5.9425 1.92678 6.41737 1.92678 6.71026 2.21967L11.9603 7.46967Z"
+          fill="currentColor"
+          fillRule="evenodd"
+          id="Icon (Stroke)"
+        />
+      </g>
+    </svg>
   );
 }
 
