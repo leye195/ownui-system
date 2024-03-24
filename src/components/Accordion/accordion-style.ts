8448889611444b888ea2ef13506e1b35038215e5
leye195 <leye195@naver.com>
2024-03-24 13:27:05 +0900

    style: update accordion base&itemStyle

--- src/components/Accordion/accordion-style.ts
+++ src/components/Accordion/accordion-style.ts
@@ -1,13 +1,13 @@
 export const baseStyle = {
-  light: "flex flex-col",
+  light: "flex flex-col overflow-hidden",
   bordered:
-    "flex flex-col rounded-[12px] border-solid border-[1px] border-gray400",
-  splitted: "flex flex-col gap-[10px]",
+    "flex flex-col rounded-[12px] border-solid border-[1px] border-gray400 overflow-hidden",
+  splitted: "flex flex-col gap-[10px] overflow-hidden",
 };
 
 export const itemStyle = {
-  light: "px-3",
-  bordered: "px-3 ",
+  light: "px-3 bg-[var(--bg-color)] transition-all hover:brightness-90",
+  bordered: "px-3 bg-[var(--bg-color)] transition-all hover:brightness-90",
   splitted:
-    "px-3 rounded-[12px] bg-white border-solid border-[1px] border-gray400",
+    "px-3 rounded-[12px] bg-[var(--bg-color)] transition-all hover:brightness-90 bg-white border-solid border-[1px] border-gray400",
 };
