a2d0bd6423772cd799d3f1dd9840e239f41b2ede
leye195 <leye195@naver.com>
2024-03-24 15:12:42 +0900

    feat: export components/hooks/types/styles into index.ts

---
+++ src/hooks/index.ts
@@ -0,0 +1,4 @@
+export * from "./useClickOutSideEffect";
+export * from "./useDomRef";
+export * from "./useMounted";
+export * from "./useIsomorphicLayoutEffect";
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
+import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "index",
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+    },
+  },
+  plugins: [react(), tsconfigPaths(), dts({ insertTypesEntry: true })],
 });
