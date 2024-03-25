3177b14a2729e345d51c394a364aac5942726e7c
leye195 <leye195@naver.com>
2024-03-25 21:43:43 +0900

    # add playground

---
+++ playground/src/App.tsx
@@ -0,0 +1,122 @@
+import {
+  Input,
+  TextField,
+  Accordion,
+  AccordionItem,
+  Button,
+  colors,
+  Drawer,
+  DrawerContent,
+  TextArea,
+} from "ownui-system";
+import { useState } from "react";
+
+function App() {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">Playground</h1>
+      <br />
+      <Input placeholder="Text" size="large" inValid />
+      <br />
+      <Input placeholder="Text" />
+      <br />
+      <Input placeholder="Text" size="small" />
+      <br />
+      <TextField placeholder="TextField" label="label" />
+      <div>
+        <Accordion defaultSelected="accordion1" variant="splitted">
+          <AccordionItem
+            body={<div>body</div>}
+            header={<div>header</div>}
+            value={"accordion2"}
+          />
+          <AccordionItem
+            body={<div>body</div>}
+            header={<div>header</div>}
+            value={"accordion1"}
+          />
+        </Accordion>
+      </div>
+      <br />
+      <div className="flex gap-2 line">
+        <Button className="text-white" color={colors.green300} size="large">
+          Button
+        </Button>
+        <Button className="text-white" color={colors.green300} size="medium">
+          Button
+        </Button>
+        <Button className="text-white" color={colors.green300} size="small">
+          Button
+        </Button>
+      </div>
+      <br />
+      <div>
+        <Button
+          className="text-white h-fit"
+          color={colors.green300}
+          size="xlarge"
+          onClick={() => setIsDrawerOpen(true)}
+        >
+          Open Drawer
+        </Button>
+        <Drawer
+          allowCloseOnClickBackDrop
+          isOpen={isDrawerOpen}
+          placement="left"
+          onOpenChange={(isOpen: boolean) => setIsDrawerOpen(isOpen)}
+        >
+          <DrawerContent>
+            <div className="p-10">Drawer Left</div>
+          </DrawerContent>
+        </Drawer>
+      </div>
+      <br />
+      <div className="p-4 flex gap-2">
+        <TextField
+          hasError
+          helpMessage="Enter your help text"
+          label="Label"
+          placeholder="Text"
+          size="large"
+        />
+        <TextField
+          hasError
+          helpMessage="Enter your help text"
+          label="Label"
+          placeholder="Text"
+        />
+        <TextField
+          hasError
+          helpMessage="Enter your help text"
+          label="Label"
+          placeholder="Text"
+          size="small"
+        />
+      </div>
+      <br />
+      <div className="p-4 flex gap-2">
+        <TextArea
+          helpMessage="Enter your help text"
+          label="Label"
+          placeholder="Text"
+        />
+        <TextArea
+          hasError
+          helpMessage="Enter your help text"
+          label="Label"
+          placeholder="Text"
+        />
+        <TextArea
+          disabled
+          helpMessage="Enter your help text"
+          label="Label"
+          placeholder="Text"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ playground/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/ownui-system/dist/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+};
