8873eae31b71bae1075113e9843b44e67962bb71
leye195 <leye195@naver.com>
2024-03-30 13:53:48 +0900

    style: adjust grabber&header size

--- src/components/BottomSheet/bottomsheet-header.tsx
+++ src/components/BottomSheet/bottomsheet-header.tsx
@@ -5,8 +5,8 @@ function BottomSheetHeader({ onPointerDown }: ComponentProps<"div">) {
   return (
     <div
       className={clsx(
-        "relative min-h-[50px] py-[4px] transition-all",
-        "after:cursor-pointer after: after:absolute after:content-[''] after:rounded-3xl after:w-[60px] after:h-[10px]  after:top-[6px] after:left-[50%] after:translate-x-[-50%]",
+        "relative min-h-[38px] py-[4px] transition-all",
+        "after:cursor-pointer after: after:absolute after:content-[''] after:rounded-3xl after:w-[45px] after:h-[8px]  after:top-[6px] after:left-[50%] after:translate-x-[-50%]",
         " after:bg-gray-300 hover:after:bg-gray-400",
       )}
       style={{
