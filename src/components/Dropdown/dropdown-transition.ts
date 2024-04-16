f66d66008f7fa0d7c03a1634017ed7e5daaa5fd9
leye195 <leye195@naver.com>
2024-04-16 21:36:36 +0900

    style: update dropdown transition

--- src/components/Dropdown/dropdown-transition.ts
+++ src/components/Dropdown/dropdown-transition.ts
@@ -2,9 +2,10 @@ export const scaleInOut = {
   enter: {
     opacity: 1,
     scale: 1,
+    z: 0,
     transition: {
       opacity: {
-        duration: 0.3,
+        duration: 0.25,
         ease: "easeInOut",
       },
     },
@@ -12,8 +13,9 @@ export const scaleInOut = {
   exit: {
     opacity: 0,
     scale: 0.9,
+    originY: "-10px",
     transition: {
-      duration: 0.3,
+      duration: 0.25,
       ease: "easeInOut",
     },
   },
