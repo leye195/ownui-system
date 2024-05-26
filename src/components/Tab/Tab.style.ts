37870972d46df1dcd2c32abac24e6b793367bd63
leye195 <leye195@naver.com>
2024-05-26 11:36:49 +0900

    refactor: add Style(s) add postfix

--- src/components/Tab/Tab.style.ts
+++ src/components/Tab/Tab.style.ts
@@ -1,33 +1,33 @@
-export const baseStyle = {
-  text: "cursor-pointer text-black font-semibold font-semibold",
-  box: "cursor-pointer text-black rounded-[14px] font-semibold",
-};
-
-export const activeStyle = {
+export const baseStyles = {
   text: "cursor-pointer text-black font-semibold",
-  box: "cursor-pointer text-white rounded-[14px] font-semibold",
+  box: "cursor-pointer text-black rounded-xl font-semibold",
 };
 
-export const tabSize = {
+export const activeStyles = {
+  text: "cursor-pointer text-black font-semibold",
+  box: "cursor-pointer text-white rounded-xl font-semibold",
+};
+
+export const tabSizeStyles = {
   text: {
     large: "text-[20px]",
     medium: "text-[16px]",
     small: "text-[14px]",
   },
   box: {
-    large: "text-[20px] px-[16px] py-[8px]",
-    medium: "text-[16px] px-[10px] py-[8px]",
-    small: "text-[14px] px-[10px] py-[8px]",
+    large: "text-[20px] px-[16px] py-2",
+    medium: "text-[16px] px-[10px] py-2",
+    small: "text-[14px] px-[10px] py-2",
   },
 };
 
-export const tabGroupBaseStyle = {
-  text: "mb-[8px] border-solid border-b-[1px] border-b-[#EBEBEB]",
-  box: "rounded-[16px] p-[4px] bg-[#F5F5F5]",
+export const tabGroupBaseStyles = {
+  text: "mb-2 border-solid border-b border-b-gray-300",
+  box: "rounded-2xl p-1 bg-gray-300",
 };
 
-export const tabGroupGap = {
-  large: "gap-[26px]",
-  medium: "gap-[22px]",
-  small: "gap-[16px]",
+export const tabGroupGapStyles = {
+  large: "gap-6",
+  medium: "gap-5",
+  small: "gap-4",
 };
