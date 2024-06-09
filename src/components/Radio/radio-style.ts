b484bd7c7c5a7ac0de6f9f23cfe9b42b4b98e658
leye195 <leye195@naver.com>
2024-06-09 12:34:16 +0900

    feat: add tailwind-merge & cn style util function

--- src/components/Radio/radio-style.ts
+++ src/components/Radio/radio-style.ts
@@ -1,11 +1,10 @@
-export const labelStyle =
-  "relative inline-block align-middle font-semibold pl-[26px]";
+export const labelStyle = 'relative flex align-middle font-semibold';
 
 export const itemBaseStyle =
-  "w-5 h-5 flex items-center justify-center ju rounded-full border border-gray-400 absolute left-0 top-[2px]";
+  'relative w-5 h-5 flex items-center justify-center ju rounded-full border-[2px] border-gray-400 left-0 top-[2px]';
 
 export const itemAfterStyle =
-  "after:transition-all after:contents-[''] after:scale-0  after:bg-[var(--bg-color)] after:w-3 after:h-3 after:absolute after:rounded-full";
+  "after:transition-all after:contents-[''] after:scale-0 after:bg-gray-900 after:w-3 after:h-3 after:absolute after:rounded-full";
 
 export const itemCheckedStyle =
-  "peer-checked:w-5 peer-checked:h-5 peer-checked:after:scale-100 peer-checked:border-[var(--bg-color)]";
+  'peer-checked:w-5 peer-checked:h-5 peer-checked:after:scale-75 peer-checked:border-gray-900';
