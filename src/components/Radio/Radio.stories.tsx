397f260fddb98d24d3a61600905c7e3a96d99b13
leye195 <leye195@naver.com>
2024-06-09 13:07:43 +0900

    feat: update radio storybook

--- src/components/Radio/Radio.stories.tsx
+++ src/components/Radio/Radio.stories.tsx
@@ -1,6 +1,7 @@
 import { colors } from "@ownui-system/styles/colors";
+import { cn } from "@ownui-system/styles/util";
 import type { Meta, StoryObj } from "@storybook/react";
-import RadioItem from "./radio-item";
+import RadioItem, { RadioItemProps } from "./radio-item";
 import Radio from ".";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
@@ -32,6 +33,51 @@ export const Default: Story = {
         <Radio.Item id="" name="test" value="test2">
           Test2
         </Radio.Item>
+      </Radio.Group>
+    );
+  },
+};
+
+export const CustomRadio: Story = {
+  args: {
+    name: "tesst",
+    id: "",
+    activeColor: "",
+    children: <></>,
+  },
+  render: () => {
+    function CustomRadio({ children, ...restProps }: RadioItemProps) {
+      return (
+        <div
+          className={cn(
+            "relative px-4 py-2 border border-gray-300 rounded-xl",
+            "flex items-center justify-between",
+            "hover:opacity-55",
+          )}
+        >
+          {children}
+          <Radio.Item {...restProps} className="static" />
+        </div>
+      );
+    }
+
+    return (
+      <Radio.Group
+        activeColor={colors.gray800}
+        className="gap-4 flex-col w-[400px]"
+      >
+        <CustomRadio id="" name="test" value="test2">
+          <div className="flex flex-col">
+            <span className="text-lg"> Free</span>
+            <span className="text-sm">Up to 100 items</span>
+          </div>
+        </CustomRadio>
+        <CustomRadio id="" name="test" value="test2">
+          <div className="flex flex-col">
+            <span className="text-lg">Pro</span>
+            <span className="text-sm">Unlimited items. $5 per month</span>
+          </div>
+        </CustomRadio>
       </Radio.Group>
     );
   },
