9e8456137ac08bd37a4f88403b2120047f409d80
leye195 <leye195@naver.com>
2024-11-17 21:17:52 +0900

    chore: update version to 0.1.4

--- src/components/SnackBar/index.tsx
+++ src/components/SnackBar/index.tsx
@@ -1,2 +1,3 @@
 export { default as SnackBarProvider } from "./snackbar-provider";
 export { default as SnackBar } from "./snackbar";
+export { default as useSnackBarContext } from "./snackbar-context";
